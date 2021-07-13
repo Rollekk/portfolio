08df3ef1e538332f39caaf2df0a12c362b957984
Rollekk <48874046+Rollekk@users.noreply.github.com>
2021-07-13 05:50:40 +0200

    Added files for new site

---
+++ js/main.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function($){
+	var isLateralNavAnimating = false;
+	
+	//open/close lateral navigation
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running 
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+			
+			$('body').toggleClass('navigation-is-open');
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
+});
+
+$(window).load(function() {
+	var $container = $('.portfolio-items');
+	$container.isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 750,
+			easing: 'linear',
+			queue: false
+		}
+	});
+	$('.cat a').click(function() {
+		$('.cat .active').removeClass('active');
+		$(this).addClass('active');
+		var selector = $(this).attr('data-filter');
+		$container.isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false
+			}
+		});
+		return false;
+	});
+
+});
